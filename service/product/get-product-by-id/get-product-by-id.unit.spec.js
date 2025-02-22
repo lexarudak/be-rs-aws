ffab5bd2e62c14675e7552acf460d131a2528665
a-rudak-EIS <arudak@ebsco.com>
2025-02-22 16:30:31 +0500

    test: fix tests

--- service/product/get-product-by-id/get-product-by-id.unit.spec.js
+++ service/product/get-product-by-id/get-product-by-id.unit.spec.js
@@ -1,5 +1,5 @@
-import { handler } from "./get-product-by-id.js";
-import data from "../__mocks__/products-list-mock.js";
+import { handler } from "./get-product-by-id";
+import data from "../__mocks__/products-list-mock";
 
 describe("get-product-by-id", () => {
 	beforeEach(() => {
@@ -8,14 +8,7 @@ describe("get-product-by-id", () => {
 
 	it("should return 200 and the product when the product is found", async () => {
 		const event = {
-			body: "",
-			headers: {},
-			httpMethod: "GET",
-			isBase64Encoded: false,
-			path: "",
-			queryStringParameters: null,
 			pathParameters: { id: "1" },
-			stageVariables: null,
 		};
 
 		const result = await handler(event);
@@ -26,14 +19,7 @@ describe("get-product-by-id", () => {
 
 	it("should return 404 when the product is not found", async () => {
 		const event = {
-			body: "",
-			headers: {},
-			httpMethod: "GET",
-			isBase64Encoded: false,
-			path: "",
-			queryStringParameters: null,
 			pathParameters: { id: "999" },
-			stageVariables: null,
 		};
 
 		const result = await handler(event);
--- service/product/get-products-list/get-products-list.unit.spec.js
+++ service/product/get-products-list/get-products-list.unit.spec.js
@@ -4,6 +4,7 @@ import data from "../__mocks__/products-list-mock";
 describe("get-products-list handler", () => {
 	it("should return a 200 status code and the correct data", async () => {
 		const response = await handler();
+		
 		expect(response.statusCode).toBe(200);
 		expect(response.headers).toEqual({
 			"Access-Control-Allow-Origin": "*",
