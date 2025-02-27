d4ec2a56f04c7a3248c76eda6fc47af396e4e38c
a-rudak-EIS <arudak@ebsco.com>
2025-02-27 14:51:57 +0500

    feat: add scripts to fill tables

---
+++ infra/scripts/dynamoDB/fill-products-table/__mocks__/products-mock.js
@@ -0,0 +1,40 @@
+const productsList = [
+    {
+        description: "Short Product Description",
+        id: "f473aa59-3ced-415a-86c4-a4c686d0ff95",
+        price: 24,
+        title: "Valera",
+    },
+    {
+        description: "Short Product Description",
+        id: "54bcff31-f83f-4816-9b7d-53e9a7709ae6",
+        price: 15,
+        title: "Natalia",
+    },
+    {
+        description: "Short Product Description",
+        id: "59e96c9d-16fa-4a80-ba86-c6618fd7b97a",
+        price: 23,
+        title: "Marina",
+    },
+    {
+        description: "Short Product Description",
+        id: "0ae4ac66-bc23-4446-a7ac-cd08a8f3ce53",
+        price: 15,
+        title: "Zinaida",
+    },
+    {
+        description: "Short Product Description",
+        id: "2daad77d-8c6f-4662-aace-0080f9e8f83c",
+        price: 23,
+        title: "Irina",
+    },
+    {
+        description: "Short Product Description",
+        id: "a05a4751-0a03-4f2c-a95b-db2ac3e6f95c",
+        price: 15,
+        title: "Olga",
+    },
+];
+
+export default productsList;
---
+++ infra/scripts/utils/constants.js
@@ -0,0 +1,6 @@
+export const DYNAMO_DB_TABLES = {
+  PRODUCTS: "products",
+  STOCKS: "stocks"
+}
+
+export const DYNAMO_DB_REGION = "eu-north-1"
