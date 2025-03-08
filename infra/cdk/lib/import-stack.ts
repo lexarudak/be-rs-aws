59da581dcc580b888127969de629b5737e7d2b1e
a-rudak-EIS <arudak@ebsco.com>
2025-03-08 23:47:20 +0500

    test: add tests

--- infra/cdk/lib/import-stack.ts
+++ infra/cdk/lib/import-stack.ts
@@ -1,17 +1,49 @@
-import { Stack, StackProps, Duration, CfnOutput } from "aws-cdk-lib";
+import { Stack, StackProps, Duration } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { Table } from "aws-cdk-lib/aws-dynamodb";
-import { DYNAMO_DB_TABLES } from "../utils/constants";
 import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Bucket, EventType } from "aws-cdk-lib/aws-s3";
+import { LambdaDestination } from "aws-cdk-lib/aws-s3-notifications";
 
 export class ImportStack extends Stack {
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
 
 		const bucketName = "rs-aws-import";
+		const importBucket = Bucket.fromBucketName(
+			this,
+			"ExistingImportBucket",
+			bucketName
+		);
+
+		const importFileParser = new NodejsFunction(this, "importFileParser", {
+			runtime: Runtime.NODEJS_22_X,
+			entry: "../../service/import/import-file-parser/import-file-parser.ts",
+			handler: "handler",
+			memorySize: 128,
+			timeout: Duration.seconds(30),
+			bundling: {
+				externalModules: ["aws-sdk"],
+			},
+		});
+
+		importFileParser.addToRolePolicy(
+			new PolicyStatement({
+				actions: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"], // Разрешения для копирования, удаления и чтения
+				resources: [
+					`${importBucket.bucketArn}/uploaded/*`, // Чтение из папки "uploaded"
+					`${importBucket.bucketArn}/parsed/*`, // Запись в папку "parsed"
+				],
+			})
+		);
+
+		importBucket.addEventNotification(
+			EventType.OBJECT_CREATED,
+			new LambdaDestination(importFileParser),
+			{ prefix: "uploaded/" }
+		);
 
 		const importProductsFile = new NodejsFunction(this, "importProductsFile", {
 			runtime: Runtime.NODEJS_22_X,
